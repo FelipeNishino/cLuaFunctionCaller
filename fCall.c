d63e532088998570f893ba8a8af2cefcf3a4c3c7
FelipeNishino <felipenishino@gmail.com>
2021-09-09 18:53:06 -0300

    Code cleanup + comments

--- fCall.c
+++ fCall.c
@@ -11,16 +11,17 @@
 
 #define VERBOSE 1
 
-/* function declaration */
 void f (lua_State* L, const char* functionName, int argCount, const char** args);
 void error (lua_State *L, const char *fmt, ...);
 
 int main(int argc, const char* argv[]){
+	// Checks the validity of args
 	if (argc < 4) {
 		printf("Quantidade invalida de argumentos.\n");
 		return 1;
 	}
 
+	// Prints received values
 	if (VERBOSE) {
 		printf("funcName: %s, argCount: %d, ", argv[1], atoi(argv[2]));
 		int i = 0;
@@ -30,42 +31,44 @@ int main(int argc, const char* argv[]){
 		printf("\n");
 	}
 
+	// Initializes and opens de lua file
 	lua_State* L = luaL_newstate();
 	luaL_openlibs(L);
 	luaL_dofile(L, "luaFunctions.lua");
 	
+	// Runs the function passed as parameters
 	f(L, argv[1], atoi(argv[2]), &argv[3]);
 	
+	// Closes the lua file
 	lua_close(L);
 	return 0;
 }
 
 void f(lua_State* L, const char* functionName, int argCount, const char** args) {
-	/* push functions and arguments */
-	lua_getglobal(L, functionName);  /* function to be called */
+	// Push function and arguments
+	lua_getglobal(L, functionName);  // Function to be called
 	int i = 0;
 	for (i = 0; i < argCount; ++i) {
 		printf("pushed Value: %f\n", atof(args[i]));
-		lua_pushnumber(L, atof(args[i]));   /* push i-th argument */
+		lua_pushnumber(L, atof(args[i]));   // push i-th argument
 	}
 
-	/* do the call (2 arguments, 1 result) */
+	// Calls the function
 	if (lua_pcall(L, argCount, 1, 0) != 0)
 	error(L, "error running function `%s': %s\n Certifique-se de que a função chamada existe no arquivo luaFunctions.lua\n", functionName,
 	          lua_tostring(L, -1));
 	
-	/* retrieve result */
-
+	// Get/treat results
 	if (lua_isnumber(L, -1)){
 		double z = lua_tonumber(L, -1);
 		lua_pop(L, 1);  /* pop returned value */
-		printf("Valor de resultado %f\n", z);
+		printf("Resultado: %f\n", z);
 	}
 	else {
-        const char* res = lua_tostring(L, -1);
+        const char* returnedVal = lua_tostring(L, -1);
         lua_pop(L, 1);  /* pop returned value */
 
-        printf("Valor de resultado: %s\n", res);
+        printf("Resultado: %s\n", returnedVal);
     }
 }
 
